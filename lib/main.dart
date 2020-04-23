c0917266e0ca082bcf1f5e9d7a34efbf3df6b480
Hari Om <58305594+Madhav008@users.noreply.github.com>
2020-04-23 19:13:01 +0530

    final

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/foundation.dart';
 import 'package:navigation_drawer/Sign_up.dart';
+import 'package:navigation_drawer/welcome.dart';
 import 'Sign_in.dart';
-import 'fancy_action.dart';
 import 'package:flutter/material.dart';
+
+import 'homepage.dart';
 
 void main() => runApp(MyApp());
 
@@ -20,79 +22,12 @@ class MyApp extends StatelessWidget {
               : null),
       initialRoute: '/',
       routes: {
-        '/': (context) => HomePage(),
+        '/': (context) => WelcomePage(),
         '/signup': (context) => SignupPage(),
-        '/signin': (context) => SignIn(),
-      },
+        '/signin': (context) => MyHomePage(),
+        '/home' : (context) => HomePage()
+            },
     );
   }
 }
 
-class HomePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Drawer App"),
-        elevation: defaultTargetPlatform == TargetPlatform.android ? 5.0 : 0.0,
-      ),
-      drawer: Drawer(
-        child: ListView(
-          children: <Widget>[
-            UserAccountsDrawerHeader(
-              accountName: Text("Madhav Jindal"),
-              accountEmail: Text("madhavjindal21@gmail.com"),
-              currentAccountPicture: CircleAvatar(
-                backgroundColor: Colors.red,
-                child: Text('MJ'),
-              ),
-              otherAccountsPictures: <Widget>[
-                CircleAvatar(
-                  backgroundColor: Colors.white,
-                )
-              ],
-            ),
-            ListTile(
-              title: Text("Page One"),
-              trailing: Icon(Icons.arrow_upward),
-            ),
-            Divider(
-              color: Colors.blueGrey,
-            ),
-            ListTile(
-              title: Text("Sign In"),
-              trailing: Icon(Icons.arrow_upward),
-              onTap: () => Navigator.of(context).pushNamed('/signin'),
-            ),
-            Divider(
-              color: Colors.blueGrey,
-            ),
-            ListTile(
-              title: Text("Sign Up"),
-              onTap: () => Navigator.of(context).pushNamed('/signup'),
-              trailing: Icon(Icons.arrow_downward),
-            ),
-            Divider(
-              color: Colors.blueGrey,
-            ),
-            ListTile(
-                title: Text("Close"),
-                trailing: Icon(Icons.close),
-                onTap: () => Navigator.of(context).pop()),
-            Divider(
-              color: Colors.blueGrey,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FancyAction(
-        onPressed: null,
-      ),
-      body: Container(
-        child: Center(
-          child: Text("HomePage"),
-        ),
-      ),
-    );
-  }
-}
